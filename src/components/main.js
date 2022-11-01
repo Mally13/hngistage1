66eda3d774cda98540ee31ce0c8ba9674330b675
Mally13 <mallymutuku13@gmail.com>
2022-11-01 15:19:15 +0300

    https

--- src/components/main.js
+++ src/components/main.js
@@ -13,7 +13,7 @@ const profiledata ={
 const data = [
     ['btn_twitter','https://twitter.com/MallyMutuku13','Twitter Link',''],
     ['btn_zuri','https://training.zuri.team/', 'Zuri Team',''],
-    ['books','http://books.zuri.team', 'Zuri Books','this is where you find books about design and coding'],
+    ['books','https://books.zuri.team', 'Zuri Books','this is where you find books about design and coding'],
     ['book__python','https://books.zuri.team/python-for-beginners?ref_id=%3Cyourslackname%3E', 'Python Books','where you feature the book as if you were selling it'],
     ['pitch','https://background.zuri.team/', 'Background Check for Coders',' where you pitch a service for doing background checks on coders'],
     ['book_design','https://books.zuri.team/design-rules', 'Design Books','where you pitch the free design book offered by Zuri'],
@@ -25,7 +25,7 @@ function Main(){
     <>
     <div className='profile'>
     <div className='share' data-text="Share Link">
-      <img id='share__img' src={share} alt='share'  onClick={()=>{navigator.clipboard.writeText("http")}}/>
+      <img id='share__img' src={share} alt='share'  onClick={()=>{navigator.clipboard.writeText("https://hngi-stage1-mary.netlify.app/")}}/>
       <img id='sharemobile__img' src={shareMobile} alt='share'  onClick={()=>{navigator.clipboard.writeText("http")}}/>
       <div className='share-overlay' data-text="Link Copied"></div>
     </div>
