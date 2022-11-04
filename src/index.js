3e1c1d7309f616af12c10c7e01d6210e2e96e128
Mally13 <mallymutuku13@gmail.com>
2022-11-04 09:11:20 +0300

    feat: react router dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Contact from './Contact';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+const router=createBrowserRouter([
+  {
+    path: '/',
+    element:<App/>
+  },
+  {
+    path: '/contact',
+    element:<Contact/>
+  }
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
